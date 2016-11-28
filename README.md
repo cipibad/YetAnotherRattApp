1a1436799dbff8375dc17c46c6c0107959b08683
Ciprian Badescu <cipibad@gmail.com>
2016-11-28 23:07:37 +0200

    small evening work

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 This app serves to add a reminder to specific transportation line and station so phone will remind you when to leave to cetch the tram just in time
 
 ## TODO
-* replace html connection with Jsoup
-* implement chose Line Id
+* replace html connection with Jsoup - DONE
+* implement chose Line Id - DONE
+* implement chose Direction - DONE
 * implement chose Station
 * implement chose Time advance
 * implement add to favourite
--- app/src/main/java/ro/cipex/yetanotherrattapp/MainActivity.java
+++ app/src/main/java/ro/cipex/yetanotherrattapp/MainActivity.java
@@ -12,24 +12,28 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private HashMap<String, String> transportationServiceUrls;
-    private static final String DEBUG_TAG = "DownloadWebpageTask";
+    public HashMap<String, String> liniiIds;
+
+    private static final String DEBUG_TAG = "AppCompatActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         transportationServiceUrls = new HashMap<String, String>();
+        liniiIds = new HashMap<String, String> ();
         transportationServiceUrls.put("Tramvai", "http://86.122.170.105:61978/html/timpi/tram.php");
 
 
@@ -63,8 +67,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
+                Log.d(DEBUG_TAG, "Network is connected, starting download task");
+
                 Log.d(DEBUG_TAG, "Network is connected, starting async task");
-                new DownloadWebpageTask(this).execute(url);
+                new DownloadWebpageTask().execute(url);
+
+                //System.setProperty("http.proxyHost", "global.proxy.alcatel-lucent.com");
+                //System.setProperty("http.proxyPort", "8000");
+
             } else {
                 Log.d(DEBUG_TAG, "Network is not connected, doing nothing");
             }
@@ -73,92 +83,147 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         }
 
+        if ( spinner.getId() == R.id.spinner_line_id ) {
+            // An item was selected. You can retrieve the selected item using
+            String choice = (String) parent.getItemAtPosition(pos);
+            Log.d(DEBUG_TAG, "Line: " + choice);
+
+            String urlLinie = "http://86.122.170.105:61978/html/timpi/" + liniiIds.get(choice);
+            Log.d(DEBUG_TAG, "Linie service URL: " + urlLinie);
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                Log.d(DEBUG_TAG, "Network is connected, starting download task");
+
+                Log.d(DEBUG_TAG, "Network is connected, starting async task");
+                new DownloadWebpageTask().execute(urlLinie);
+
+                //System.setProperty("http.proxyHost", "global.proxy.alcatel-lucent.com");
+                //System.setProperty("http.proxyPort", "8000");
+
+            } else {
+                Log.d(DEBUG_TAG, "Network is not connected, doing nothing");
+            }
+
+
+
+        }
+
+        }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-    }
-
-
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    private class DownloadWebpageTask extends AsyncTask<String, Void, Void> {
         private static final String DEBUG_TAG = "DownloadWebpageTask";
 
-        AppCompatActivity parentActivity;
-
-        public DownloadWebpageTask(AppCompatActivity activity) {
-            parentActivity = activity;
-        }
-        // Given a URL, establishes an HttpUrlConnection and retrieves
-// the web page content as a InputStream, which it returns as
-// a string.
-        private String downloadUrl(String myurl) throws IOException {
-            InputStream is = null;
-            // Only display the first 500 characters of the retrieved
-            // web page content.
-            int len = 500;
-
-            try {
-                URL url = new URL(myurl);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                // Starts the query
-                conn.connect();
-                int response = conn.getResponseCode();
-                Log.d(DEBUG_TAG, "The response is: " + response);
-                is = conn.getInputStream();
-
-                // Convert the InputStream into a string
-                String contentAsString = readIt(is, len);
-                return contentAsString;
-
-                // Makes sure that the InputStream is closed after the app is
-                // finished using it.
-            } finally {
-                if (is != null) {
-                    is.close();
-                }
-            }
-        }
-
-
-        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-            Reader reader = null;
-            reader = new InputStreamReader(stream, "UTF-8");
-            char[] buffer = new char[len];
-            reader.read(buffer);
-            return new String(buffer);
-        }
+        Document doc;
 
         @Override
-        protected String doInBackground(String... urls) {
+        protected Void doInBackground(String... params) {
 
-            // params comes from the execute() call: params[0] is the url.
             try {
-                return downloadUrl(urls[0]);
+                Log.d(DEBUG_TAG, "Downloading" + params[0]);
+                doc = Jsoup.connect(params[0]).get();
             } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
+                e.printStackTrace();
             }
+
+            return null;
         }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
 
 
-            Spinner lineIdSpinner = (Spinner) findViewById(R.id.spinner_line_id);
+        protected void onPostExecute(Void result) {
 
-            String [] lineIds = new String [] { result };
+            String title = doc.title();
 
-            ArrayAdapter adapter = new ArrayAdapter(parentActivity, android.R.layout.simple_spinner_item, lineIds);
+            if (! doc.select("#apDiv5").isEmpty()) {
+                Elements titleJSOUP = doc.select("#apDiv5").select("a");
+                Spinner lineIdSpinner = (Spinner) findViewById(R.id.spinner_line_id);
+                Log.d(DEBUG_TAG, "Parsing" + titleJSOUP.toString());
 
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            // Apply the adapter to the spinner
-            lineIdSpinner.setAdapter(adapter);
+                List<String> lineIds = new ArrayList<String>();
+                //String[] lineIds = new String[];
+                for (Element src : titleJSOUP) {
+                    liniiIds.put(src.attr("title"), src.attr("href"));
+                    lineIds.add(src.attr("title"));
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item, lineIds);
+
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                lineIdSpinner.setAdapter(adapter);
+                lineIdSpinner.setOnItemSelectedListener(MainActivity.this);
+            }
+
+            if (! doc.select("table[bgcolor=FFA500]").isEmpty()) {
+                Log.d(DEBUG_TAG, "Parsing Line info");
+
+                Elements sensuri = doc.select("table[bgcolor=FFA500]").select("td").select("b");
+                Log.d(DEBUG_TAG, "Sensuri " + sensuri.toString());
+
+
+                List<String> sensList = new ArrayList<String>();
+                //String[] lineIds = new String[];
+                for (Element sens : sensuri) {
+                    sensList.add(sens.text());
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item, sensList);
+
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // Apply the adapter to the spinner
+                Spinner spinner = (Spinner) findViewById(R.id.spinner_sens);
+
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(MainActivity.this);
+
+            }
+//
+//
+//            Elements maine;
+//            Elements titleJSOUP;
+//            Elements recipeJSOUP;
+//            Elements instructionsJSOUP;
+//
+//            String recipE;
+//
+//            maine = doc.select("div#recipeContent");
+//
+//            titleJSOUP = doc.select("title");
+//
+//            recipeJSOUP = maine.select("ul.recipe");
+//
+//            instructionsJSOUP = maine.select("p.instructions");
+//
+//
+//
+//            recipE = recipeJSOUP.toString();
+//
+//
+//            drinkNameText.setText("THE "
+//                    + Jsoup.parse(titleJSOUP.toString()).text()
+//            );
+//
+//
+//
+//            dontListenText.setText(Jsoup.parse(titleJSOUP.toString()).text()
+//            );
+//
+//            recipeText.setText(prepareDRINK(recipE));
+//
+//            instructionsText.setText(Jsoup.parse(instructionsJSOUP.toString())
+//                    .text());
+//
+//            dialog.dismiss();
+//
+//
         }
-    }
-
-
-}
+    };
+};
